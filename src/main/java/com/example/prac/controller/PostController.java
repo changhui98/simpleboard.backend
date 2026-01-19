6ba9cfe17504808f74b230e5a9fee8328b0e9b25
changhui98 <net_0102@naver.com>
2026-01-19 20:39:13 +0900

    Feat : cors config

--- src/main/java/com/example/prac/controller/PostController.java
+++ src/main/java/com/example/prac/controller/PostController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/post")
+@RequestMapping("/api/posts")
 @RequiredArgsConstructor
 public class PostController {
 
