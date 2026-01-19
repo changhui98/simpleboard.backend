3d10adaad418a2d2daf9d20a226f7a986b1a22c9
changhui98 <net_0102@naver.com>
2026-01-19 10:46:30 +0900

    Feat : Post Controller 생성

---
+++ src/main/java/com/example/prac/controller/PostController.java
@@ -0,0 +1,78 @@
+package com.example.prac.controller;
+
+import com.example.prac.model.Post;
+import com.example.prac.service.PostService;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/post")
+@RequiredArgsConstructor
+public class PostController {
+
+    private final PostService postService;
+
+    @GetMapping
+    public ResponseEntity<Page<Post>> listPost(
+        @RequestParam(defaultValue = "0") int page,
+        @RequestParam(defaultValue = "10") int size
+    ) {
+        Pageable pageable = PageRequest.of(page,size);
+        Page<Post> posts = postService.getPosts(pageable);
+
+        return ResponseEntity.ok().body(posts);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Post> getPost(
+        @PathVariable Long id
+    ) {
+        Optional<Post> postOpt = postService.getPost(id);
+
+        return postOpt.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Post> createPost(
+        @RequestBody Post post
+    ) {
+        Post createPost = postService.createPost(post);
+
+        return ResponseEntity.ok(createPost);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Post> updatePost(
+        @PathVariable Long id,
+        @RequestBody Post updatePost
+    ) {
+        Optional<Post> updated = postService.updatePost(id, updatePost);
+
+        return updated.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePost(
+        @PathVariable Long id
+    ) {
+        boolean deleted = postService.deletePost(id);
+        if (deleted) {
+            return ResponseEntity.ok().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
